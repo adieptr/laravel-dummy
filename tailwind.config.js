9bd103ce3652cfd405a7b0a1d732f2634c8b7334
adieptr <adie6809@gmail.com>
2025-08-06 11:17:16 +0700

    tambah landing page dan custom logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                primary: '#000033',
+                secondary: '#ffce09'
+              },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
